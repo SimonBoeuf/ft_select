4835be22f9408b8f87595d1641005ce62ad70c68
kasoutami <kasoutami@gmail.com>
2014-01-05 19:31:07 +0100

    modif cursor

--- cursor_fct.c
+++ cursor_fct.c
@@ -1,16 +1,5 @@
 #include "ft_select.h"
 
-void	init_cursor(t_cursor *cursor)
-{
-	char	*res;
-
-	cursor->pos_x = 0;
-	cursor->pos_y = 0;
-	res = tgetstr("cm", NULL);
-	tputs(tgoto(res, cursor->pos_x, cursor->pos_y), 1, ft_putchar);
-	free(res);
-}
-
 void	move_cursor(t_cursor *cursor, int x, int y)
 {
 	char	*res;
