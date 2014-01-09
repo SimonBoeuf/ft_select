7b74ae18e10d5ac35d67e8871690269ac9eea64c
Moss-id <id.moss@hotmail.fr>
2014-01-09 14:03:39 +0100

    Resize fixed. Now the resize don't init a new term at each resize.

--- sighandler_fct.c
+++ sighandler_fct.c
@@ -22,8 +22,19 @@ void	catch_int(void)
 
 void	catch_resize(void)
 {
+	t_list				*list;
+	t_cursor			*cursor;
+	struct winsize		w;
+	t_elem				*tmp;
+
 	tputs(tgetstr("cl", NULL), 1, ft_putchar);
-	init_sequence();
+	list = ft_getlist(0, NULL);
+	tmp = list->curr_elem;
+	w = ft_get_winsize();
+	ft_print_list(list, w.ws_row - 1);
+	list->curr_elem = tmp;
+	cursor = init_cursor(list->curr_elem->posx, list->curr_elem->posy);
+	move_cursor(cursor);
 }
 
 void	catch_terminate(void)
